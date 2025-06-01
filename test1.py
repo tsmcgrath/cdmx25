ef969702e4de8973dd04ec510bcb194db86d94db
Tim McGrath <tim@wanderfeast.com>
2025-06-01 09:03:15 -0700

    Content for first draft
    Content for first draft

--- test1.py
+++ test1.py
@@ -1,37 +1,20 @@
 # from the readme at: https://github.com/FABallemand/ezGPX
 
 import ezgpx
-import matplotlib
+import matplotlib.pyplot as plt
 
 
 # config
 gpxpath = "/Users/tim/DocumentsLocal/Github/cdmx25/data/"
-
+testplt_gpx = plt.subplots()
 # Parse GPX file
-gpx = ezgpx.GPX(gpxpath + "activity_18627254105.gpx")
+newgpx = ezgpx.GPX(gpxpath + "TacoRide.gpx")
 
 # Simplify (using Ramer-Dougle-Peucker algorithm)
-gpx.simplify()
+newgpx.simplify()
 
 # Remove metadata
-gpx.remove_metadata()
-
-# Plot with Matplotlib
-test_gpx.matplotlib_plot(figsize=(16,9),
-                         size=6,
-                         color="ele",
-                         cmap=matplotlib.cm.get_cmap("gnuplot", 12),
-                         colorbar=False,
-                         start_point_color="green",
-                         stop_point_color="red",
-                         way_points_color=None,
-                         background="World_Imagery",
-                         offset_percentage=0.04,
-                         dpi=100,
-                         title=test_gpx.name(),
-                         title_fontsize=20,
-                         watermark=True,
-                         file_path="img_1.png")
+newgpx.remove_metadata()
 
 # Write new simplified GPX file
-gpx.to_gpx(gpxpath + "new_file.gpx")
+newgpx.to_gpx(gpxpath + "new_TacoRide.gpx")
