58c7b1dcb7d7bec11f80a74d3fc110a364f72aeb
Tim McGrath <tim@wanderfeast.com>
2025-08-13 10:10:58 -0700

    Got test4.py working. Reading GPX to GeoPandas dataframe. Plus filtering for non-empty layer in GPX.
    Got test4.py working. Reading GPX to GeoPandas dataframe. Plus filtering for non-empty layer in GPX.

--- test4.py
+++ test4.py
@@ -1,14 +1,30 @@
 import geopandas as gpd
+import pyarrow
 import os
 import matplotlib.pyplot as plt
+import fiona
+from fiona import Geometry, Feature, Properties
 
 # config
-gpxpath = "/Users/tim/DocumentsLocal/Github/cdmx25/data/"
-testplt_gpx = plt.subplots()
+gpx_path = "/Users/tim/DocumentsLocal/Github/cdmx25/data-in/"
+# testplt_gpx = plt.subplots()
 
 # Read the GPX file
-gpx_file = (gpxpath + "TacoRide.gpx")
+gpx_file = (gpx_path + "LuckyLake.gpx")
 
 # List available layers
-layers = fiona.listlayers(gpx_path)
+layers = fiona.listlayers(gpx_file)
+# Filter for only non-empty layers
+non_empty_layers = []
+
+for layer in layers:
+    try:
+        gdf = gpd.read_file(gpx_file, layer=layer)
+        print(f"Layer '{layer}' contains {len(gdf)} features.")
+        if not gdf.empty:
+            non_empty_layers.append(layer)
+    except Exception as e:
+        print(f"Error reading layer '{layer}': {e}")
+
 print("Available layers:", layers)
+print("Non-empty layers:", non_empty_layers)
