2f0d80d6b42273cdbe427fd9daea27ca4c05066e
Tim McGrath <tim@wanderfeast.com>
2025-09-05 09:05:33 -0700

    Can't use polars. Removed null columns
    Can't use polars. Removed null columns

---
+++ test8.py
@@ -0,0 +1,36 @@
+import geopandas as gpd
+import pyarrow
+import os
+import matplotlib.pyplot as plt
+import fiona
+from fiona import Geometry, Feature, Properties
+
+# config
+gpx_path = "/Users/tim/DocumentsLocal/Github/cdmx25/data-in/"
+out_path = "/Users/tim/DocumentsLocal/Github/cdmx25/data-out/"
+# testplt_gpx = plt.subplots()
+
+# Read the GPX file
+gpx_file = (gpx_path + "TacoRide.gpx")
+outfile_root = "TacoRide"
+
+# List available layers
+layers = fiona.listlayers(gpx_file)
+# Filter for only non-empty layers
+non_empty_layers = []
+
+for layer in layers:
+    try:
+        gdf = gpd.read_file(gpx_file, layer=layer)
+        print(f"Layer '{layer}' contains {len(gdf)} features.")
+        if not gdf.empty:
+            non_empty_layers.append(layer)
+            outfile = str(out_path + outfile_root + "-" + layer + ".geojson")
+            # Drop columns where *any* value is null
+            gdf_cleaned = gdf.dropna(axis=1, how='any')
+            gdf_cleaned.to_file(outfile, driver='GeoJSON')
+    except Exception as e:
+        print(f"Error reading layer '{layer}': {e}")
+
+print("Available layers:", layers)
+print("Non-empty layers:", non_empty_layers)
