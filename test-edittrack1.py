196f8773479fac6d6e004d55c637b5b3c7b1edc7
Tim McGrath <tim@wanderfeast.com>
2025-09-28 11:22:09 -0700

    New tests. Getting Timezone converted to local for more editing/weeding of points at stops.
    New tests. Getting Timezone converted to local for more editing/weeding of points at stops.

--- test-edittrack1.py
+++ test-edittrack1.py
@@ -1,13 +1,18 @@
+import pandas as pd
 import geopandas as gpd
 import pyarrow
 import os
 import matplotlib.pyplot as plt
 import fiona
 from fiona import Geometry, Feature, Properties
+from datetime import datetime, UTC
+from zoneinfo import ZoneInfo
 
 # config
 gpx_path = "/Users/tim/DocumentsLocal/Github/cdmx25/data-in/"
 out_path = "/Users/tim/DocumentsLocal/Github/cdmx25/data-out/"
+# 1. Create a ZoneInfo object for Mexico City
+mexico_city_tz = ZoneInfo('America/Mexico_City')
 # testplt_gpx = plt.subplots()
 
 # Read the GPX file
@@ -29,6 +34,10 @@ for layer in layers:
             outfile = str(out_path + outfile_root + "-" + layer + ".geojson")
             # Drop columns where *any* value is null
             gdf_cleaned = gdf.dropna(axis=1, how='any')
+            # Convert UTC time to local time
+            gdf_cleaned['local-dtime'] = pd.to_datetime(gdf_cleaned['time'])
+            gdf_cleaned['local-dtime'] = gdf_cleaned['local-dtime'].dt.tz_convert(mexico_city_tz)
+            # Output GeoJson
             gdf_cleaned.to_file(outfile, driver='GeoJSON')
     except Exception as e:
         print(f"Error reading layer '{layer}': {e}")
